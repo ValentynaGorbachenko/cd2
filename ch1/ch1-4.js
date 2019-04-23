623565727db71c9cb188049e50e15d4f34a8d1e5
ValentynaGorbachenko <gorbachenko.valentyna@gmail.com>
2019-04-22 22:32:57 -0700

    add javascript solution to chapter 1

--- ch1/ch1-4.js
+++ ch1/ch1-4.js
@@ -2,3 +2,22 @@
 /*
 For [1,3,5,7,9,13], print values that are greater than its 2nd value. 
 */
+function valueGreaterThanSecond(arr){
+	let arrRes = []
+	if(arr.length >1 && typeof(arr[1]) == "number"){
+		for ( let i=0; i<arr.length;i++){
+			if (typeof(arr[i]) == "number"){
+				if (arr[i]>arr[1]){
+					arrRes.push(arr[i])
+				}
+			}
+		}
+	}
+	console.log(arrRes)
+}
+
+valueGreaterThanSecond([])
+valueGreaterThanSecond([2])
+valueGreaterThanSecond([5, 2])
+valueGreaterThanSecond([5, 2, 3])
+valueGreaterThanSecond(["a", 2, 4, "6", 3])
