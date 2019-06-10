1dc5c1de84f23616401a9c441947efa4bcf2526f
ValentynaGorbachenko <gorbachenko.valentyna@gmail.com>
2019-06-10 00:19:02 -0700

    update fib and factorial with iterative solution

--- ltcd/factorial.py
+++ ltcd/factorial.py
@@ -4,7 +4,7 @@ factorial of the given number.  For example, factorial(5) should return 120.
 Do this using recursion; remember that factorial(n) = n * factorial(n-1).
 '''
 def factorial(n):
-    if n == 1:
+    if n == 1 or n == 0:
         return 1
     if n == 2:
         return 2
@@ -12,3 +12,13 @@ def factorial(n):
     return n*factorial(n-1)
 
 print(factorial(5))
+
+def factorial_iter(n):
+    res = 1
+    for i in range(1, n+1):
+        print('before ', res, i)
+        res *= i
+        print('after ', res, i)
+
+    return res
+print(factorial_iter(5))
--- ltcd/fib.py
+++ ltcd/fib.py
@@ -10,4 +10,18 @@ def fib(n):
         return 1
     return fib(n-2) + fib(n-1)
 
-print (fib(5))
+print (fib(12))
+
+def fir_iter(n):
+    i, j = 0, 1
+    for x in range(0, n):
+        # print('before ', i, j)
+        i, j = j, j+i
+        # print('after ', i, j)
+        # swap
+        # temp = i
+        # i = j
+        # j = temp + j
+    return i
+
+print(fir_iter(5))
