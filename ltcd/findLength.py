1fb8ad8bb46ab6008ae403946f30625dde0fb3c3
ValentynaGorbachenko <gorbachenko.valentyna@gmail.com>
2019-06-24 12:21:43 -0700

    add brute force for find lengthproblem

--- ltcd/findLength.py
+++ ltcd/findLength.py
@@ -26,7 +26,7 @@ def findLength(A,B):
         try:
             print("in try")
             j=B.index(el)
-
+            
             while A[i] == B[j]:
                 print('A[i], B[j] is ', A[i], ", ", B[j])
                 count+=1
@@ -45,9 +45,49 @@ def findLength(A,B):
     return max_count
 # findLength([1,2,3,2,1],[3,2,1,4,7])
 # findLength([0,1,1,1,1,0],[1,1,1,1])
-findLength([0,0,0,0,0,0,1,0,0,0],
-           [0,0,0,0,0,0,0,1,0,0])
+# findLength([0,0,0,0,0,0,1,0,0,0],[0,0,0,0,0,0,0,1,0,0])
 
+
+
+def findLength2(A,B):
+    count=0
+    max_count=0
+
+    for i, el in enumerate(A):
+        # print(i,el)
+        for j, el_j in enumerate(B):
+            print('i is ',i, ', el is ',el, ', j is ',j, ', el_j is ', el_j)
+            ii = i
+            try:
+                print("in try")
+                # j=B.index(el)
+                # # while I can find index of el try checking next index of el
+                # ind = j
+
+                while A[ii] == B[j]:
+                    print('A[ii], B[j] is ', A[ii], ", ", B[j])
+                    count+=1
+                    ii+=1
+                    j+=1
+                # print ('ind is ', ind, B[ind+1:])
+                # ind=B[ind+1:].index(el)
+                # print ('ind is ',ind)
+                
+            except:
+                # continue
+                # break
+                pass
+                print("index out of range")
+            print('conut is ',count)
+            if max_count<count:
+                max_count=count
+            count=0
+    print("max is ", max_count)
+    return max_count
+
+findLength2([1,2,3,2,1],[3,2,1,4,7])
+findLength2([0,1,1,1,1,0],[1,1,1,1])
+findLength2([0,0,0,0,0,0,1,0,0,0],[0,0,0,0,0,0,0,1,0,0])
 
 
 
