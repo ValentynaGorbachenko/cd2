c2bd0087e8358fdc720554253662fb5b24b68639
ValentynaGorbachenko <gorbachenko.valentyna@gmail.com>
2019-07-01 12:06:33 -0700

    add remove element problem

--- ltcd/moveZeroes.py
+++ ltcd/moveZeroes.py
@@ -16,8 +16,7 @@ def moveZeroes(nums):
     nums.extend([0]*cnt)
     print(nums)
     j=0
-    for i in range(len(nums)-cnt):
-        
+    for i in range(len(nums)-cnt):    
         i+=j
         print(i, nums[i])
         if nums[i] == 0:
@@ -25,16 +24,8 @@ def moveZeroes(nums):
             j-=1
         
 
-    # while i < len(nums):
-    #     print(i)
-    #     if nums[i] == 0:
-    #         nums.append(0)
-    #         # nums.pop(i)
-    #         # i -= 1
-
-    #     i+=1
-
     print(nums)
+
 
 moveZeroes([0,0,0,0,1,0,3,12])
 
